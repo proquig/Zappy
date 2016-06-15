06759b05c3b82078232597fa3b23c301426b9a8d
Guillaume PROQUIN <guillaume.proquin@epitech.eu>
2016-06-15 13:36:39 +0100

    t_player

--- server/include/player.h
+++ server/include/player.h
@@ -2,26 +2,28 @@
 // Created by cloquet on 14/06/16.
 //
 
-#ifndef ZAPPY_SERVER_PLAYER_H
-#define ZAPPY_SERVER_PLAYER_H
+#ifndef				ZAPPY_SERVER_PLAYER_H
+# define			ZAPPY_SERVER_PLAYER_H
 
-enum			Direction
+# include			<stdlib.h>
+
+enum				Direction
 {
   UP,
   DOWN,
-  RIGHT,
-  LEFT
+  LEFT,
+  RIGHT
 };
 
 typedef struct		s_ressources
 {
-  int 			linemate;
-  int 			deraumere;
-  int	 		sibur;
-  int 			mendiane;
-  int 			phiras;
-  int			thystame;
-}			t_ressources;
+  int 				linemate;
+  int 				deraumere;
+  int	 			sibur;
+  int 				mendiane;
+  int 				phiras;
+  int				thystame;
+}					t_ressources;
 
 typedef struct		s_player
 {
@@ -29,15 +31,20 @@ typedef struct		s_player
   unsigned int 		y;
   enum Direction 	dir;
   t_ressources		ress;
-  int 			fd;
-  int 			team;
-  int 			lvl;
-  t_player		*next;
-}			t_player;
+  int 				fd;
+  int 				team;
+  int 				lvl;
+  struct s_player	*next;
+}					t_player;
 
 typedef struct 		s_command
 {
-  char *		cmd;
-  void 			(*f)();
-}			t_command;
+  char *			cmd;
+  void 				(*f)();
+}					t_command;
+
+t_player			*init_player(int fd);
+t_player			*add_player(t_player *list, t_player *player);
+t_player			*del_player(t_player *list, t_player *player);
+
 #endif //ZAPPY_SERVER_PLAYER_H
--- server/src/main.c
+++ server/src/main.c
@@ -16,6 +16,65 @@ int		main(int ac, const char **av)
   t_param	param;
   t_env		env;
 
+  /*
+  //
+  t_player *list;
+  t_player *tmp;
+  list = init_player(1);
+  tmp = list;
+  while (tmp)
+  {
+	printf("%d\n", tmp->fd);
+	tmp = tmp->next;
+  }
+  printf("END STEP1\n");
+  list = del_player(list, list);
+  tmp = list;
+  while (tmp)
+  {
+	printf("%d\n", tmp->fd);
+	tmp = tmp->next;
+  }
+  printf("END STEP2\n");
+  list = init_player(0);
+  t_player *player1 = init_player(1);
+  t_player *player2 = init_player(2);
+  add_player(list, player1);
+  add_player(list, player2);
+  tmp = list;
+  while (tmp)
+  {
+	printf("%d\n", tmp->fd);
+	tmp = tmp->next;
+  }
+  printf("END STEP3\n");
+  list = del_player(list, list);
+  tmp = list;
+  while (tmp)
+  {
+	printf("%d\n", tmp->fd);
+	tmp = tmp->next;
+  }
+  printf("END STEP4\n");
+  list = del_player(list, player1);
+  tmp = list;
+  while (tmp)
+  {
+	printf("%d\n", tmp->fd);
+	tmp = tmp->next;
+  }
+  printf("END STEP5\n");
+  list = del_player(list,player2);
+  tmp = list;
+  while (tmp)
+  {
+	printf("%d\n", tmp->fd);
+	tmp = tmp->next;
+  }
+  printf("END STEP6\n");
+  return (0);
+   */
+  //
   if (ac < 7)
     error("argv");
   init_params(&param);
