83b358f5c9b31da37417da557dac4eaab1555d87
Cloquet <alban.cloquet@epitech.eu>
2016-06-18 13:34:03 +0200

    Give me a break, it's 2am.  But it works now.

---
+++ client/include/SocketBuf.hpp
@@ -0,0 +1,51 @@
+//
+// Created by cloquet on 18/06/16.
+//
+
+#ifndef CLIENT_SOCKETBUF_HPP
+#define CLIENT_SOCKETBUF_HPP
+
+#include <streambuf>
+#include <iostream>
+#include <fcntl.h>
+
+class 		Socket
+{
+  int 		_fd_socket;
+  std::string 	_command;
+
+ public:
+  Socket(){};
+
+  ~Socket()
+  { };
+
+  void	setFdSocket(int fd)
+  {
+    _fd_socket = fd;
+  }
+
+  std::string &read()
+  {
+    FILE *f;
+    char *buf;
+    size_t len;
+    int a;
+
+    buf = NULL;
+    if (!(f = fdopen(this->_fd_socket, "r")))
+      perror("fdopen");
+    if ((a = getline(&buf, &len, f)) == -1)
+      exit(0);
+    buf[a] = 0;
+    _command = buf;
+    free(buf);
+    return _command;
+  };
+
+  void write()
+  { }
+};
+
+
+#endif //CLIENT_SOCKETBUF_HPP
